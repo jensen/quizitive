3349a3d4398833580c75be2494181a0735391939
Karl Jensen <karljensen@gmail.com>
2021-12-16 01:32:02 -0800

    Disable links for non authed

--- src/pages/quizzes/list.tsx
+++ src/pages/quizzes/list.tsx
@@ -9,38 +9,46 @@ interface IQuizItemProps extends IQuizWithUser {
   disabled: boolean;
 }
 
-const QuizItem = (props: IQuizItemProps) => (
-  <li className="w-full px-4 py-1 hover:border-l-4 border-pink-800">
-    <Link
-      className="hover:grayscale flex justify-between items-center"
-      to={props.slug}
-    >
-      <div>
-        <div className="flex space-x-2 items-end">
-          <h2 className="text-xl text-white">{props.name}</h2>
-          <h3 className="text-md text-pink-800"> by </h3>
-          <h2 className="text-xl text-white">{props.user.name}</h2>
-        </div>
-        <h4 className="text-sm text-pink-900">
-          {format(new Date(props.created_at), "MMMM do, yyyy")}
-        </h4>
+const QuizItem = (props: IQuizItemProps) => {
+  const content = (
+    <div>
+      <div className="flex space-x-2 items-end">
+        <h2 className="text-xl text-white">{props.name}</h2>
+        <h3 className="text-md text-pink-800"> by </h3>
+        <h2 className="text-xl text-white">{props.user.name}</h2>
       </div>
-      {props.disabled === false && (
-        <h1 className="text-4xl font-bold text-pink-800 hover:text-white">
-          GO
-        </h1>
+      <h4 className="text-sm text-pink-900">
+        {format(new Date(props.created_at), "MMMM do, yyyy")}
+      </h4>
+    </div>
+  );
+
+  return (
+    <li className="w-full px-4 py-1 hover:border-l-4 border-pink-800">
+      {props.disabled === false ? (
+        <Link
+          className="hover:grayscale flex justify-between items-center"
+          to={props.slug}
+        >
+          {content}
+          <h1 className="text-4xl font-bold text-pink-800 hover:text-white">
+            GO
+          </h1>
+        </Link>
+      ) : (
+        content
       )}
-    </Link>
-    {props.owner && (
-      <Link
-        to={`${props.slug}/edit`}
-        className="text-sm text-pink-900 hover:text-pink-400"
-      >
-        Edit
-      </Link>
-    )}
-  </li>
-);
+      {props.owner && (
+        <Link
+          to={`${props.slug}/edit`}
+          className="text-sm text-pink-900 hover:text-pink-400"
+        >
+          Edit
+        </Link>
+      )}
+    </li>
+  );
+};
 
 export default function QuizList() {
   const auth = useAuth();
@@ -59,6 +67,11 @@ export default function QuizList() {
           />
         ))}
       </ul>
+      {auth.user === null && (
+        <p className="mt-8 text-sm text-pink-800 w-full flex justify-center">
+          Must login to participate in a quiz.
+        </p>
+      )}
     </div>
   );
 }
